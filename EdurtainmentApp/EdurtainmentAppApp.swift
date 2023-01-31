0d60cfefd3c5c3e60f477e275d59443ddca7c688
Shahid Arshad <shahid.arshad@devsinc.com>
2023-01-31 17:07:44 +0500

    Initial Commit

---
+++ EdurtainmentApp/EdurtainmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdurtainmentAppApp.swift
+//  EdurtainmentApp
+//
+//  Created by Dev on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct EdurtainmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
